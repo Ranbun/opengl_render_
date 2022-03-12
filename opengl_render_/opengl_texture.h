bb8ce3cb98a930bbc3a0d79cb801ca6163a76b94
RanLH <granbao@gmail.com>
2022-03-13 00:48:11 +0800

    add new file
    create a texture

---
+++ opengl_render_/opengl_texture.h
@@ -0,0 +1,63 @@
+#ifndef OPENGL_TEXTURE_H
+#define OPENGL_TEXTURE_H
+
+#include <iostream>
+#include <string>
+#include <glad/glad.h>
+#include <GL/GL.h>
+#include "render_en.h"
+#include "stb_image.h"
+
+class Texture
+{
+public:
+	explicit Texture(std::string& path);  // 读取文件 
+
+	~Texture();
+
+	// bind 
+	void bindTexture() const;
+
+	//create 
+	void createTexture();
+
+	// set wrap & filter
+	void setWarpParameteri(const texture::WRAP wrap, unsigned int type);
+	void setFilterParameteri(texture::FILETER filter, unsigned int type);
+
+	// release
+	void releaseTexture();
+
+	// 翻转图像
+	static void flipVerticallyImage();
+
+private:
+	
+	// 紋理的的图片信息
+	int width_;
+	int height_;
+	// 通道信息 
+	int nr_channels_;
+	// 调用create之前暂存的 图像数据 
+	unsigned char* data_;  //暂存 像素的数据
+
+	// wrap 
+	GLuint wrap_s_;
+	GLuint wrap_t_;
+
+	// filter 
+	GLuint filter_min_;
+	GLuint filter_mag_;
+
+	// texture ID  
+	unsigned int texture_id_;
+
+public:
+
+	// 使用 stb_image 读取一个纹理 返回纹理的ID 
+	static unsigned int textureFromFile(std::string path, std::string& directory,bool gamma = false);
+};
+
+
+
+#endif // !1
