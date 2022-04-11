4860234074be729147f9ea9c5760b87230d31e93
RanLH <granbao@gmail.com>
2022-04-11 22:40:19 +0800

    remove

--- opengl_render_ project/README.md
+++
@@ -1,5 +0,0 @@
-### OpenGL Render
-
-- 一个`OpenGL`环境的简单封装, `USE:`
-  - `glfw` +`glad` + `glm`
-
--- opengl_render_ project/stb_image.cpp
+++
@@ -1,7 +0,0 @@
-#ifndef LOAD_IMAGE_H
-#define LOAD_IMAGE_H
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-#endif // !1
