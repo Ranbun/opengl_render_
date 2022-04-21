a3171005ea0447365c350d92568e63cd3309f531
RanLH <granbao@gmail.com>
2022-04-21 20:26:47 +0800

    remove file

--- deferredshading/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "deferredshading.h"
-int main()
-{
-    DeferredShading::initialize(gl_version_major::major_3, gl_version_minor::minor_3);
-    DeferredShading app;
-    app.run();
-
-    return 0;
-}
